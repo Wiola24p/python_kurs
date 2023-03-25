8b66cdfd8c5ed438351dbb9674d74bee69d49858
Wiola <wiola24p@o2.pl>
2023-03-25 13:30:35 +0100

    krok3 turtle

--- lab3/main1.py
+++ lab3/main1.py
@@ -1,7 +1,13 @@
-from metoda_monte_carlo import approximate_pi
+import simple_draw as sd
+
+
 def main():
     #print(rvp("((2 + 7) /3 + (8 - 3)*4)/2"))
     #print(solve("2 7 + 3 / 14 3 - 4 * + 2 /"))
-    print(approximate_pi())
+    #print(approximate_pi())
+
+    sd.draw_rect()
+
+
 if __name__== "__main__":
     main()
