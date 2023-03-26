117400fd55412efc2bae5f23be7be23cb5d7681f
Wiola <wiola24p@o2.pl>
2023-03-26 09:26:09 +0200

    krok4

--- lab3/l_system.py
+++ lab3/l_system.py
@@ -1,7 +1,9 @@
 import turtle
 def apply(c):
-    if c=="F":
-        return "F+F-F-F+F"
+    if c=="A":
+        return "B-A-B"
+    elif c=="B:"
+        return"A+B+A"
     else:
         return c
 
@@ -20,20 +22,39 @@ def create_l_system(n, start):
 
 def draw_l_system(t,instructions, angle, lenght):
     for c in instructions:
-        if c=="F":
+        if c==["A", "B"]:
             t.forward(lenght)
         elif c=="-":
             t.right(angle)
         elif c=="+":
             t.left(angle)
+   t.pu()
+            t.goto(position)
+            t.setheading(heading)
+            t.pd()
 
+def draw():
+    screen = turtle.Screen()
+    t = turtle.Turtle()
+
+    t.speed(100)
+    instructions = create_l_system(4, "X")
+    draw_l_system(t,instructions,25,2)
+    t.left(90)
+    t.pu()
+    t.back(200)
+    t.pd()
+    t.speed(5)
+    instructions = create_l_system(5, "X")
+    draw_l_system(t,instructions,25,5)
+    screen.exitonclick()
 
 def draw():
     screen = turtle.Screen()
     t= turtle.Turtle()
 
     t.speed(1)
-    instructions=create_l_system(3,"F")
-    draw_l_system(t, instructions, 90, 10)
+    instructions=create_l_system(8,"A")
+    draw_l_system(t, instructions, 60, 2)
 
     screen.exitonclick()
