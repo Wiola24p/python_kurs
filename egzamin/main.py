f24c3261ef816aa0038c46d7579faefbdf0c5ba7
Wiola <wiola24p@o2.pl>
2023-04-15 11:48:10 +0200

    krok3 i 4

--- egzamin/main.py
+++ egzamin/main.py
@@ -2,8 +2,8 @@ from produkty import Produkt
 
 def main():
     Produkt.wczytaj_produkty("ceny.csv")
-    print(Produkt.produkt("Ser twarogowy - za 1 kg").get_nazwa())
+    print(Produkt.produkt("Ser twarogowy - za 1 kg").cena(1,2021))
 
 
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+     main()
